004e362ce116051aaa4d15b2b055b8001bc93b42
im.anggasaputra <im.anggasaputra@gmail.com>
2023-07-08 15:58:30 +0700

    add basic auth middleware

--- app/middleware.go
+++ app/middleware.go
@@ -1,1 +1,21 @@
 package app
+
+import (
+	"net/http"
+
+	"github.com/IbnAnjung/movie_fest/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func BasicAuth(username, password string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authUsername, authPass, ok := c.Request.BasicAuth()
+		if !ok || authUsername != username || authPass != password {
+			utils.ErrorResponse(c, http.StatusUnauthorized, "Unauthorized")
+			c.Abort()
+		}
+
+		c.Next()
+
+	}
+}
